555d38b8b87657207509cd15d3597d92be604d30
Alex Rykov <alex.rykov@gmail.com>
2012-08-18 09:21:48 -0400

    simplified dependencies by loading WebLogic interdependencies in

--- beatomvn.sh
+++ beatomvn.sh
@@ -1,30 +1,162 @@
 #!/bin/bash
-#classpathdep=unzip -q -c weblogic.jar META-INF/MANIFEST.MF | sed ':a;N;$!ba;s/\n //g'|grep Class-Path|tr ' ' '\n'
-#unzip -q -c weblogic.jar META-INF/MANIFEST.MF | sed ':a;N;$!ba;s/\n //g'|tr ' ' '\n'|grep modules
+#default values
+defaultversion=1036
+groupid='com.bea.weblogic'${defaultversion}
+unixname=`uname -o`
+printNameVersion(){
+  jar=$1
+  #echo "<!--"
+  #cat META-INF/MANIFEST.MF 
+  #echo "-->"
+  bundleName=$(cat META-INF/MANIFEST.MF | grep 'Bundle-SymbolicName:' | sed 's/Bundle-SymbolicName://'|sed 's/ //g')
+  bundleVersion=$(cat META-INF/MANIFEST.MF | grep 'Bundle-Version' | sed 's/Bundle-Version://'|sed 's/ //g')  
+  if [ "${bundleName}" =  "" ]
+    then
+      artifactId=`echo \`basename $jar\`|sed 's/\.jar//'`      
+    else
+      artifactId=${bundleName}      
+  fi
+  if [ "${bundleVersion}" = "" ]
+    then
+      version=${defaultversion}
+    else
+      version=${bundleVersion}      
+  fi
+  #always default version
+  version=${defaultversion}
+  
+  
+  echo '
+        <artifactId>'${artifactId}'</artifactId>
+        <version>'${version}'</version>'
+}
+
+extractManifest(){
+   file=${1}
+   if [ "$unixname" = "Cygwin" ]
+     then
+          file="$(cygpath -w ${file})"
+     fi  
+   
+   
+   jar xvf ${file} META-INF/MANIFEST.MF > /dev/null
+   
+}
+
+createPom(){
+  jar=$1
+  
+  echo '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+      <modelVersion>4.0.0</modelVersion>
+      <groupId>'${groupid}'</groupId>
+      <packaging>jar</packaging>'
+  
+  extractManifest ${jar}
+  printNameVersion ${jar}
+  jardir=`dirname ${jar}`   
+  
+  echo '<dependencies>'
+  dependencies=$(cat META-INF/MANIFEST.MF | sed ':a;N;$!ba;s/\n //g'|grep "Depends-On"|tr ' ' '\n'|grep -v "Depends-On" |grep -v "^$") 
+  if [ "${dependencies}" = "" ]
+  then
+    #CLASSPATH dependencies    
+    dependencies=$(cat META-INF/MANIFEST.MF | sed ':a;N;$!ba;s/\n //g'|grep Class-Path|tr ' ' '\n'|grep jar)    
+    
+    for dependency in ${dependencies}
+    do    
+      dependencypath=${jardir}'/'${dependency}    
+      if [ -a ${dependencypath} ]
+        then
+          echo '
+          <dependency>
+            <groupId>'${groupid}'</groupId>'            
+            extractManifest ${dependencypath}            
+            printNameVersion ${dependencypath}
+          echo '
+          </dependency>'
+    
+      fi
+    done
+  else
+    #Depends-On dependencies
+    if [ "$jar" != "$jardir/com.oracle.weblogic.rac.ucp_1.1.0.0.jar" ]
+    then
+      for dependency in $dependencies
+      do
+        dependencyAndVersion=(${dependency//;version=/ })        
+        artifactId=${dependencyAndVersion[0]}
+        
+        if [  ${#dependencyAndVersion[@]} -eq 1 ]
+        then
+          version='1.0'
+        else
+          version=${dependencyAndVersion[1]}
+        fi
+        #always default version
+        version=${defaultversion}
+        #invalid dependency. would not want this resolved
+        if [[ $artifactId =~ .*=.* ]]
+        then
+          echo "
+             <!--skipping ${artifactId}:${version}-->"
+        else
+          echo '
+            <dependency>
+              <groupId>'${groupid}'</groupId>            
+              <artifactId>'${artifactId}'</artifactId>
+              <version>'${version}'</version>          
+            </dependency>'
+          fi
+            
+      
+        
+      done
+    fi
+    
+  fi
+  echo '</dependencies></project>'
+}
+
+loadJar() {
+  f=${1}
+  pomFile='/tmp/pom.xml'
+  createPom "${f}" > ${pomFile}
+
+  if [ "$unixname" = "Cygwin" ]
+  then
+        f="$(cygpath -w ${f})"
+        pomFile="$(cygpath -w ${pomFile})"
+  fi
+  
+  mvn install:install-file -Dfile="${f}" -DpomFile=${pomFile}  
+
+}
+#check BEA_HOME that was passed if it was passed
 if [ ! -d $1/modules ] || [ ! -f $1/wlserver_10.3/server/lib/weblogic.jar ]
 then
   echo "Usage: $0 <BEA_HOME>"
-  exit 1
+    exit 1
 fi
 
-unixname=`uname -o`
-weblogic_jar=$1/wlserver_10.3/server/lib/weblogic.jar
-if [ "$unixname" = "Cygwin" ]
-  then
-    weblogic_jar="$(cygpath -w $weblogic_jar)"
-  fi
-mvn install:install-file -DgroupId=weblogic -DartifactId=weblogic -Dversion=10.3 -Dpackaging=jar -Dfile=$weblogic_jar
-for f in $1/modules/*.jar; 
-do 
-  echo "Processing $f file.."; 
-  f1=`echo \`basename $f\`|sed 's/\.jar//'|sed 's/_/ /'`
-  arr=($f1)
-  echo Artifact ID: ${arr[0]}
-  echo Version: ${arr[1]}
-  if [ "$unixname" = "Cygwin" ]
-  then
-    f="$(cygpath -w $f)"
-  fi
-  mvn install:install-file -DgroupId=weblogic -DartifactId=${arr[0]} -Dversion=${arr[1]} -Dpackaging=jar -Dfile=$f
 
+#load all the jars you can find
+#for f in $(find ${1}/modules/. -name "*.jar")
+#do
+#  loadJar ${f}
+#done
+#
+#for f in $(find ${1}/wlserver_10.3/. -name "*.jar")
+#do
+#  loadJar ${f}
+#done
+#
+
+for f in $(find ${1}/. -name "com.oracle.oraclepki_1.0.0.0.jar")
+do
+  loadJar ${f}
+done
+for f in $(find ${1}/. -name "com.oracle.weblogic.rac.ucp_1.1.0.0.jar")
+do
+  loadJar ${f}
 done
